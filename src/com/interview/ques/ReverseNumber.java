e303bed257f072f7ac4a5593052b7797a4f5f5fe
SachinClavrit <108923341+SachinClavrit@users.noreply.github.com>
2022-12-28 17:09:13 +0530

    Initial commit

---
+++ src/com/interview/ques/ReverseNumber.java
@@ -0,0 +1,15 @@
+package com.interview.ques;
+
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        int num=123;
+        int rev=0;
+        while(num!=0) {
+            int last = num % 10;
+            rev= rev *10+last;
+            num=num/10;
+        }
+        System.out.println(rev);
+    }
+}
